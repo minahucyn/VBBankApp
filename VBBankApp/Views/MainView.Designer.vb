277f0aa8b377eaa0937c1d1c95b7b538fdeb080f
Moomina Hussain <moominahussein@outlook.com>
2020-07-12 00:31:09 +0500

    MdiContainer set True

--- VBBankApp/Views/MainView.Designer.vb
+++ VBBankApp/Views/MainView.Designer.vb
@@ -22,10 +22,19 @@ Partial Class MainView
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.SuspendLayout()
+        '
+        'MainView
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(954, 490)
+        Me.IsMdiContainer = True
+        Me.Name = "MainView"
+        Me.RightToLeftLayout = True
+        Me.Text = "Bank"
+        Me.ResumeLayout(False)
+
     End Sub
 
 End Class
