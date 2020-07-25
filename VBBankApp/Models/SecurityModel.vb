1196cbad196d37a369184166b8c3a1af38537f73
Moomina Hussain <moominahussein@outlook.com>
2020-07-25 19:53:50 +0500

    Added securities model to for implementing secutities for credits

---
+++ VBBankApp/Models/SecurityModel.vb
@@ -0,0 +1,6 @@
+﻿Public Class SecurityModel
+    Public Property Id As Integer
+    Public Property CreditId As Integer
+    Public Property Description As String
+    Public Property Worth As Double
+End Class
