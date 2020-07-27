b042d4ab6dbfac492228d66c00828c578224c21e
Moomina Hussain <moominahussein@outlook.com>
2020-07-28 04:36:09 +0500

    updated RoleSpecificMenusTable

--- BankData/dbo/Tables/RoleSpecificMenus.sql
+++ BankData/dbo/Tables/RoleSpecificMenus.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[RoleSpecificMenus]
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
-	[MenuJson] VARCHAR(100) NOT NULL,
+	[MenuJson] VARCHAR(500) NOT NULL,
 	[RolesId] INT NOT NULL, 
     CONSTRAINT [FK_RoleSpecificMenus_Roles] FOREIGN KEY ([RolesId]) REFERENCES [dbo].[Roles]([Id]),
 
