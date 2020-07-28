b06f952278942ba98a3f5063d2a1a5e6f11e3e43
Moomina Hussain <moominahussein@outlook.com>
2020-07-28 06:12:41 +0500

    Debugged stored procedure ReadAuthDetails

---
+++ BankData/dbo/StoredProcedures/usp_ReadAuthDetails.sql
@@ -0,0 +1,30 @@
+﻿CREATE PROCEDURE [dbo].[usp_ReadAuthDetails]
+	@Username VARCHAR(100)
+AS
+BEGIN
+	SET NOCOUNT ON;
+	--variables
+	DECLARE @RoleClaimsCSV varchar(500)
+	DECLARE @RolesId int
+
+	--get rolesId
+	SELECT @RolesId = [RolesId] FROM [dbo].[UserDetails] WHERE [Username] = @Username;
+	--Get claims
+	SELECT @RoleClaimsCSV = STRING_AGG([c].[Description],',')
+	FROM [dbo].[RoleClaims] [rc]
+	INNER JOIN [dbo].[Claims] [c] ON [rc].[ClaimsId] = [c].[Id]
+	WHERE [rc].[RolesId] = @RolesId;
+
+	--Select all data
+	SELECT [u].[Fullname]
+	,[ud].[Username]
+	,[r].[Description] AS [UserRole]
+	,[ud].[PasswordHash] AS [GoodHash]
+	,[ud].[IsLockedOut] AS [IsLocked]
+	,[rsm].[MenuJson]
+	,@RoleClaimsCSV AS [RoleClaimsCSV]
+	FROM [dbo].[Users] [u]
+	INNER JOIN [dbo].[UserDetails] [ud] ON [u].[Id] = [ud].[UsersId]
+	INNER JOIN [dbo].[Roles] [r] ON [u].[Id] = [ud].[RolesId]
+	INNER JOIN [dbo].[RoleSpecificMenus] [rsm] ON [rsm].[RolesId] = [r].[Id];
+END
